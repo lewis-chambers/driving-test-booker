17c45a2c89141f2b54364fe67c663004ab88099b
Lewis Chambers <lewis.n.chambers@gmail.com>
2022-12-27 23:26:54 +0000

    Added classes for dates

--- src/new_script.py
+++ src/new_script.py
@@ -1,16 +1,80 @@
-from my_logger import Logger
+from src.my_logger import Logger
 import os
+import undetected_chromedriver
+from datetime import datetime
 class App:
 
     def __init__(self):
         """Initialises the searcher."""
 
         self.log_dir = os.path.join(os.path.dirname(__file__), "logs")
-        self.logger = Logger(self.log_dir, __name__)
-        self.logger.info("Log started")
+        self.logger = Logger(__name__)
+
+        self.driver = self.get_driver()
+
+    def get_driver(self):
+        driver = undetected_chromedriver.Chrome()
+        self.logger.info("Started new chromedriver.")
+        return driver
+
+class DateRange:
+    """Represented a range of two increasing dates."""
+
+    def __init__(self, start: str, end: str, format: str='%d/%m/%Y %H:%M:%S'):
+        """Initialises the object.
+        
+        Args:
+            start: Start time.
+            end: End time.
+            format: `datetime` format used.
+        
+        Returns:
+            None.
+        """
+        self.start = datetime.strptime(start, format)
+        self.end = datetime.strptime(end, format)
+
+        if self.start > self.end:
+            raise ValueError("Start date must be before end date.")
+    
+    def is_valid_datetime(self, date: datetime) -> bool:
+        """Checks if date is between start and end.
+        
+        Args:
+            date: Query datetime.
+        Returns:
+            True or false.
+        """
+
+        if not isinstance(date, datetime):
+            raise TypeError("`date` must be a `datetime` object.")
+        
+        if self.start <= date <= self.end:
+            return True
+
+        return False
+
+class Date:
+    """Represents a date."""
+
+    def __init__(self, date_string: str, format: str = "%d/%m/%Y"):
+        """Instantiates the class.
+        
+        Args:
+            date_str: String of the date.
+            format: Format of the date string
+        
+        Returns:
+            None.
+        """
+
+        datetime_obj = datetime.strptime(date_string, format)
+        datetime_obj = datetime_obj.replace(hour=0, minute=0, second=0, microsecond=0)
+        
+        self.date = datetime_obj
 
 def main():
-    app = App()
+    app = Date("01/02/2022")
 
     a=1
 if __name__ == "__main__":
---
+++ src/tests/test_app.py
@@ -0,0 +1,66 @@
+import unittest
+from src.new_script import DateRange, Date
+from datetime import datetime
+class TestDateRangeClass(unittest.TestCase):
+
+    def test_str_to_date_returns_right_date(self):
+
+        start = "01/02/2022 10:01:02"
+        end = "01/04/2022 10:01:02"
+
+        date_obj = DateRange(start, end)
+
+        self.assertEqual(str(date_obj.start), "2022-02-01 10:01:02")
+        self.assertEqual(str(date_obj.end), "2022-04-01 10:01:02")
+
+    def test_error_if_end_before_start(self):
+
+        end = "01/02/2022 10:01:02"
+        start = "01/04/2022 10:01:02"
+
+        with self.assertRaises(ValueError):
+            date_obj = DateRange(start, end)
+
+    def test_true_if_date_query_between_dates(self):
+
+        start = "01/02/2022 10:01:02"
+        end = "01/04/2022 10:01:02"
+
+        date_obj = DateRange(start, end)
+
+        format = '%d/%m/%Y %H:%M:%S'
+        between = date_obj.is_valid_datetime(
+            datetime.strptime("01/02/2022 10:01:02", format)
+        )
+        before = date_obj.is_valid_datetime(
+            datetime.strptime("01/02/2021 10:01:02", format)
+        )
+        after = date_obj.is_valid_datetime(
+            datetime.strptime("01/02/2023 10:01:02", format)
+        )
+        on_start = date_obj.is_valid_datetime(date_obj.start)
+        on_end = date_obj.is_valid_datetime(date_obj.end)
+
+        self.assertTrue(between, "Should be true if between dates.")
+        self.assertFalse(before, "Should be false if before start")
+        self.assertFalse(after, "Should be false if after end.")
+        self.assertTrue(on_start, "Should be true if on start time")
+        self.assertTrue(on_end, "Should be true if on end time.")
+
+class TestDateClass(unittest.TestCase):
+
+    def test_datetime_returned(self):
+        
+        obj = Date("01/02/2022")
+
+        self.assertIsInstance(obj.date, datetime)
+
+
+    def test_datetime_time_is_zero(self):
+        
+        obj = Date("01/02/2022 01:02:01", format = "%d/%m/%Y %H:%M:%S")
+
+        self.assertEqual(obj.date.hour, 0)
+        self.assertEqual(obj.date.minute, 0)
+        self.assertEqual(obj.date.second, 0)
+        self.assertEqual(obj.date.microsecond, 0)
